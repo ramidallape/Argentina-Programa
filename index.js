cc8963b5dae484f3d2dc49ebae4a5494eda302c1
ramidallape <122623148+ramidallape@users.noreply.github.com>
2023-01-31 02:02:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let readMore_btn = document.getElementById('readMore_btn');
+let hideText = document.getElementById('hideText');
+
+readMore_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+  hideText.classList.toggle('showText');
+
+  if(hideText.classList.contains('showText')) {
+    readMore_btn.innerHTML = 'Ver menos'
+  }
+  else {
+    readMore_btn.innerHTML = 'Ver más'
+  }
+}
